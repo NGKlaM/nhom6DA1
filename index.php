4a116e61ac6d66372c20c0809dd54f318a385d38
cubovip2k2 <hoangngoc4t@gmail.com>
2023-03-25 01:16:32 +0700

    code chuyển từ html sang php 25/03

---
+++ index.php
@@ -0,0 +1,3 @@
+<?php include_once './side/header.php' ?>
+<?php include './side/index.php' ?>
+<?php include_once './side/footer.php' ?>
---
+++ side/footer.php
@@ -0,0 +1,36 @@
+    
+    <footer>
+        <h3 class="the_h3">Liên hệ & hỗ trợ khách hàng</h3>
+            <hr>
+        <div class="footer--content">
+            <div class="footer--content-box1">
+                <h3>HỆ THỐNG thegioialo Shop</h3><br>
+                <b>SHOWROOM BÁN HÀNG</b><br>
+                <b>Địa chỉ:</b> 10 đường 1 đồng tháp , Huyện Đan Phượng, Thành Phố Hà Nội<br>
+                <b>Hotline:</b> 0982563208<br>
+                <b>TRUNG TÂM BẢO HÀNH VÀ SỬA CHỮA SHOP</b><br>
+                <b>Địa chỉ:</b> 530 đường 3 tháng 2 , Huyện Đạn Phượng, TP Hà Nội<br>
+                <b>Hotline hỗ trợ kỹ thuật:</b> 0247896568 - 0989386588<br>
+                THỜI GIAN LÀM VIỆC<br>
+                <b>Showroom:</b> Tất cả các ngày ( 8:30 - 21:00 )<br>
+                <b>TT Bảo hành:</b> Thứ 2 - thứ 7 ( 8:30 - 17:00 )
+            </div>
+            <div class="footer--content-box2">
+                <h3>HỖ TRỢ - DỊCH VỤ</h3><br>
+                Liên hệ & hỗ trợ khách hàng
+            </div>
+            <div class="footer--content-box3">
+                <h3>HỖ TRỢ THANH TOÁN</h3><br>
+                <img src="./public/images/payments.png" alt="">
+                <img src="./public/images/post1.png" alt=""><br>
+                <img src="./public/images/dathongbao.png" alt="" width="170px">
+            </div>
+        </div>
+    </footer>
+
+   
+    <script src="public/js/app.js">
+        
+    </script>
+</body>
+</html>
